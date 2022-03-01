48e72ece3b333a3c3c429432abf8dc32d9b048e2
Wyatt Saint <WSaint@techsola.com>
2022-03-01 13:12:23 -0500

    Eliminated extraneous usings

--- techsolaWorkClockTimer/TechsolaClock.cs
+++ techsolaWorkClockTimer/TechsolaClock.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
 
 namespace techsolaWorkClockTimer
 {
